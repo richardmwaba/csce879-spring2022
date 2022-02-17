033c2e72d4992114e9e50ae0667e2ae646893593
Junzjx <79999441+Junzjx@users.noreply.github.com>
2022-02-17 13:17:36 -0600

    Update model_FMNIST.py

--- hw1/model_FMNIST.py
+++ hw1/model_FMNIST.py
@@ -3,19 +3,16 @@ from keras.models import Sequential
 from util_MNIST import *
 from keras.layers import Dense
 
-
-model = tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(64, tf.nn.relu))
-model.add(tf.keras.layers.Dense(
-        128, tf.nn.relu,
-        kernel_regularizer=tf.keras.regularizers.L1(0.01),
-        activity_regularizer=tf.keras.regularizers.L2(0.01)))
-model.add(tf.keras.layers.Dense(
-        256, tf.nn.relu,
-        kernel_regularizer=tf.keras.regularizers.L1(0.01),
-        activity_regularizer=tf.keras.regularizers.L2(0.01)))
-model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
-optimizer = tf.keras.optimizers.Adam()
-
-
+def model1 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, tf.nn.relu,))
+    model.add(tf.keras.layers.Dense(
+            256, tf.nn.relu,
+            kernel_regularizer=tf.keras.regularizers.L1(0.01),
+            activity_regularizer=tf.keras.regularizers.L2(0.01)))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    optimizer = tf.keras.optimizers.Adam()
+    
+    return model, optimizer
 
