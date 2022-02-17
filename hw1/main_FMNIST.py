28b783d18c8273b4c4c6436c2edb1b26e28301fb
Junzjx <79999441+Junzjx@users.noreply.github.com>
2022-02-16 20:44:53 -0600

    Update main_FMNIST.py

--- hw1/main_FMNIST.py
+++ hw1/main_FMNIST.py
@@ -1,1 +1,54 @@
+import tensorflow as tf
+import numpy as np
 
+from tqdm import tqdm
+from model_MNIST import *
+from util_MNIST import *
+
+loss_values = []
+train_accuracy_values = []
+
+for epoch in range(5):
+    for batch in tqdm(train_ds):
+        with tf.GradientTape() as tape:
+            # run network
+            x = tf.reshape(tf.cast(batch['image'], tf.float32), [-1, 784])/255
+            labels = batch['label']
+            logits = model(x)
+
+            # calculate loss
+            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)    
+        loss_values.append(loss)
+    
+        # gradient update
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    
+        # calculate accuracy
+        predictions = tf.argmax(logits, axis=1)
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
+        train_accuracy_values.append(accuracy)
+        
+print(model.summary())
+    
+# accuracy
+print("Accuracy:", np.mean(train_accuracy_values))
+
+vali_accuracy_values = []
+
+# Loop through one epoch of data
+for epoch in range(1):
+    for batch in tqdm(validation_ds):
+
+        x = tf.reshape(tf.cast(batch['image'], tf.float32), [-1, 784])/255
+        labels = batch['label']
+        logits = model(x)
+
+        predictions = tf.argmax(logits, axis=1)
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(predictions, labels), tf.float32))
+        vali_accuracy_values.append(accuracy)
+
+print(model.summary())
+    
+# accuracy
+print("Accuracy:", np.mean(vali_accuracy_values))
