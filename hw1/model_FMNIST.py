4b7ec34cd07c483bee21c22942fd7435432a2ace
Junzjx <79999441+Junzjx@users.noreply.github.com>
2022-02-18 22:21:38 -0600

    Update model_FMNIST.py

--- hw1/model_FMNIST.py
+++ hw1/model_FMNIST.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from keras.models import Sequential
 from util_MNIST import *
-from keras.layers import Dense, Flatten, Input
+from keras.layers import Dense, Flatten, Input, Dropout
 
 def model1 ():
     model = tf.keras.Sequential()
@@ -12,7 +12,27 @@ def model1 ():
     return model
 
 
+def model2 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(Dropout(0.2))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
 def model3 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(Dropout(0.5))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model4 ():
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
     model.add(tf.keras.layers.Dense(256, tf.nn.relu))
@@ -24,8 +44,18 @@ def model3 ():
 def model5 ():
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
-    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(Dropout(0.2))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model6 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(Dropout(0.5))
     model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
     
     return model
@@ -34,7 +64,61 @@ def model5 ():
 def model7 ():
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model8 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(Dropout(0.2))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model9 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(Dropout(0.5))
+    model.add(tf.keras.layers.Dense(64, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model10 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
     model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model11 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(Dropout(0.2))
+    model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
+    
+    return model
+
+
+def model12 ():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Dense(256, tf.nn.relu))
+    model.add(Dropout(0.5))
     model.add(tf.keras.layers.Dense(256, tf.nn.relu))
     model.add(tf.keras.layers.Dense(10, tf.nn.softmax))
     
