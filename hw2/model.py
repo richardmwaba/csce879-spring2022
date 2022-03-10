6713b526d36d75d1173aa1a933f743441d91e685
Nate Thach <nate.thach@huskers.unl.edu>
2022-03-09 22:06:48 -0600

    lstm_attention model (not working)

--- hw2/model.py
+++ hw2/model.py
@@ -19,6 +19,47 @@ def lstm_rnn(train_ds, **kwargs):
     return model
 
 
+def lstm_attention(train_ds, **kwargs):
+    attended_values = attention(train_ds)
+
+    model = Sequential([
+        attention(train_ds),
+        tf.keras.layers.LSTMCell(256),
+        tf.keras.layers.LSTMCell(64),
+        Dense(1)
+    ])
+
+    return model
+
+
+def attention(train_ds, **kwargs):
+    
+    MAX_TOKENS=5000
+    MAX_SEQ_LEN=128
+    vectorize_layer = tf.keras.layers.experimental.preprocessing.TextVectorization(
+    max_tokens=MAX_TOKENS,
+        output_mode='int',
+        output_sequence_length=MAX_SEQ_LEN)
+    VOCAB_SIZE = len(vectorize_layer.get_vocabulary())
+    EMBEDDING_SIZE = int(np.sqrt(VOCAB_SIZE))
+    embedding_layer = tf.keras.layers.Embedding(VOCAB_SIZE, EMBEDDING_SIZE)
+    query_layer = tf.keras.layers.Conv1D(filters=100, kernel_size=4, padding='same')
+    value_layer = tf.keras.layers.Conv1D(filters=100, kernel_size=4, padding='same')
+    attention = tf.keras.layers.Attention()
+    concat = tf.keras.layers.Concatenate()
+
+    # Vectorize
+    encoder = Encoder(train_ds)
+#     embeddings = Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True)
+    embeddings = embedding_layer(vectorize_layer(train_ds))
+    query = query_layer(embeddings)
+    value = value_layer(embeddings)
+    query_value_attention = attention([query, value])
+    attended_values = concat([query, query_value_attention])
+
+    return attended_values
+    
+
 def gru_rnn(train_ds, **kwargs):
     
     encoder = Encoder(train_ds)
