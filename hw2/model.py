5bfd7e67c7c81091f4c3b1f6d6579c920f765d4c
Richard <richard.w.mwaba@gmail.com>
2022-03-12 23:54:40 -0600

    Submission

--- hw2/model.py
+++ hw2/model.py
@@ -5,37 +5,6 @@ from keras.models import Sequential
 from keras.layers import TextVectorization, Embedding, Bidirectional, LSTM, GRU, Dense, Dropout, Attention, Concatenate
 
 
-def lstm_rnn(train_ds, **kwargs):
-    dense_units = kwargs['dense_units'] if 'dense_units' in kwargs else 64
-    
-    encoder = Encoder(train_ds)
-    model = Sequential([
-        encoder,
-        Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
-        Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),  # return_sequences needs to be True for stacking subsequent LSTM layers
-        Bidirectional(tf.keras.layers.LSTM(32)), # stack 2nd LSTM layer
-        Dense(dense_units, activation='relu'),
-        Dropout(0.5),
-        Dense(1)
-    ])
-    return model
-
-
-def gru_rnn(train_ds, **kwargs):
-    dense_units = kwargs['dense_units'] if 'dense_units' in kwargs else 64
-    
-    encoder = Encoder(train_ds)
-    model = Sequential([
-        encoder,
-        Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
-        Bidirectional(tf.keras.layers.GRU(64,  return_sequences=True)),
-        Bidirectional(tf.keras.layers.GRU(32)), # stack 2nd GRU layer
-        Dense(dense_units, activation='relu'),
-        Dropout(0.5),
-        Dense(1)
-    ])
-    return model
-
 def lstm_attention(train_ds, **kwargs):
     dense_units = kwargs['dense_units'] if 'dense_units' in kwargs else 64
     
